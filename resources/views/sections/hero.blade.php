3b05dea5088abc841ed486ddc3df04a32383c356
ranzdhika <94164759+RANZEIN@users.noreply.github.com>
2025-05-30 15:58:25 +0700

    Updates hero section content and image
    Changes the hero section's text and adds links to the app stores.
    Also, replaces the hero image with a WebP format for optimization.

--- resources/views/sections/hero.blade.php
+++ resources/views/sections/hero.blade.php
@@ -4,18 +4,24 @@
             <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                 <div class="position-relative left-hero-color">
                     <h1 class="mb-0 fw-bold">
-                        Discover Your Visual Investment Manager
+                        Sahabat Perjalanan Al-Quran
                     </h1>
-                    <p>The project management tool you love.</p>
-                    <a href="#portfolio" class="btn btn-warning btn-hover-secondery">
-                        <span class="d-inline-block me-2"><i class="ti ti-playstation-triangle"></i></span>
-                        Discover this Video
-                    </a>
+                    <p>Belajar Qur’an lebih mendalam bersama guru profesional<br>dan teknologi Virtual Reality.</p>
+                        <a href="https://apps.apple.com/id/app/go-ngaji/id6740255632" target="_blank" rel="noopener noreferrer">
+                            <span class="d-inline-block me-2">
+                                <img src="/assets/images/hero/App-Store.svg" alt="Download on App Store" style="height: 50px;">
+                            </span>
+                        </a>
+                        <a href="https://play.google.com/store/search?q=go%20ngaji&c=apps&hl=id" target="_blank" rel="noopener noreferrer">
+                            <span class="d-inline-block me-2">
+                                <img src="/assets/images/hero/Play-Store.svg" alt="Download on Play Store" style="height: 50px;">
+                            </span>
+                        </a>
                 </div>
             </div>
             <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                 <div class="position-relative right-hero-color">
-                    <img src="{{ asset('assets/images/hero/right-image.svg') }}" class="img-fluid" alt="Hero Image">
+                    <img src="{{ asset('assets/images/hero/right-image.webp') }}" class="img-fluid" alt="Hero Image">
                 </div>
             </div>
         </div>
